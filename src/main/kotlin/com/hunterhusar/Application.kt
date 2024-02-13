b2c6750dceeb11164d63d6a084d83398abb0790b
Hunter Husar <hunter@hunterhusar.net>
2024-02-12 21:51:00 -0600

    wip

--- src/main/kotlin/com/hunterhusar/Application.kt
+++ src/main/kotlin/com/hunterhusar/Application.kt
@@ -23,7 +23,7 @@ import io.ktor.server.plugins.cors.routing.*
 
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
